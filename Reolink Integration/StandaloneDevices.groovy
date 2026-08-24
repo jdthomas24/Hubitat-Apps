086004958123a1c5b39f64dfe96d5d8b54c7f424
Jason D Thomas <jdthomas24@yahoo.com>
2026-08-24 18:45:49 -0500

    Bump version to 1.4.5 in StandaloneDevices.groovy

--- Reolink Integration/StandaloneDevices.groovy
+++ Reolink Integration/StandaloneDevices.groovy
@@ -1,6 +1,6 @@
 /**
  * Reolink Standalone Devices (Internal Group Driver)
- * Version: 1.4.4
+ * Version: 1.4.5
  *
  * NOT user-facing. Created and managed automatically by the Reolink
  * Integration parent app -- exactly ONE instance total, shared across every
