3440093957d692b32061d6bda58bfae3c6078d6b
Jason D Thomas <jdthomas24@yahoo.com>
2026-08-24 18:45:21 -0500

    Update version number to 1.4.5

--- Reolink Integration/ReolinkDeviceBridge.groovy
+++ Reolink Integration/ReolinkDeviceBridge.groovy
@@ -1,6 +1,6 @@
 /**
  * Reolink Device Bridge (Internal Parent Driver)
- * Version: 1.4.4
+ * Version: 1.4.5
  *
  * NOT user-facing. Created and managed automatically by the Reolink
  * Integration parent app -- ONE instance per SOURCE (Hub/NVR or standalone).
