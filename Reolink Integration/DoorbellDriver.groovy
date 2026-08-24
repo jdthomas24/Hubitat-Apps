65abc30ae32a2e2973cd52af136646e5e39c09b0
Jason D Thomas <jdthomas24@yahoo.com>
2026-08-24 18:44:30 -0500

    Bump version to 1.4.5 in DoorbellDriver.groovy

--- Reolink Integration/DoorbellDriver.groovy
+++ Reolink Integration/DoorbellDriver.groovy
@@ -1,6 +1,6 @@
 /**
  * Reolink Doorbell (Component Driver)
- * Version: 1.4.4
+ * Version: 1.4.5
  *
  * Same delegation pattern as Reolink Camera, plus a "visitor" (button press)
  * event so Rule Machine can trigger straight off "pushed 1" for a doorbell
